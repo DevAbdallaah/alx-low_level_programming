4611dcfebd8865b786c51fb4e4445a757d5003c2
DevAbdallaah <Seebawaiabdallah3@gmail.com>
2023-03-17 21:55:42 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-        char i;
-        for(i='0'; i<='9'; i++)
+        int i;
+        for(i=0; i<=9; i++)
         {
-                putchar(i);
+                putchar(i+ '0');
         }
         putchar('\n');
 
