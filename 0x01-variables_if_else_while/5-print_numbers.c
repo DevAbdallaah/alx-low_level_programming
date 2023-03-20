2a07aca1c0f95d61077cd25831a69394da563aed
DevAbdallaah <Seebawaiabdallah3@gmail.com>
2023-03-19 17:38:37 -0700

    0 to 9

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,14 +7,13 @@
  */
 int main(void)
 {
-	int i;
-	for(i=0; i<10; i++)
-	{
-		printf("%d", i);
-	}
-        printf("\n");
-
-	return (0);
+int i;
+for (i = 0; i < 10; i++)
+{
+	printf("%d", i);
+}
+printf("\n");
+return (0);
 }
 
 
