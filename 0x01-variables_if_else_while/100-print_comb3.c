5e38bb2c4c1a4626e35cdad9cda0a2faefa48a51
DevAbdallaah <Seebawaiabdallah3@gmail.com>
2023-03-19 18:37:19 -0700

    Numbers without repeating

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-    int first, second;
-
-    for (first = 0; first < 10; first++)
-    {
-        for (second = 0; second < 10; second++)
-        {
-            putchar(first + '0');
-            putchar(second + '0');
-            if (first && second)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+int first;
+int second;
+for (first = 0; first <= 9; first++)
+{
+	for (second = first + 1; second <= 9; second++)
+	{
+		putchar(first + '0');
+		putchar(second + '0');
+		putchar(',');
+		putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
 }
