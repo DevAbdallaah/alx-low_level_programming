cbef736c9bd19cdddbee4d6b957d59469810e263
DevAbdallaah <Seebawaiabdallah3@gmail.com>
2023-03-22 07:41:41 -0700

    Add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+
+/**
+ * add - function add two numbers
+ * @i: is parameter
+ * @j: is parameter
+ *
+ * Return : the addition of two numbers
+ */
+
+int add(int i, int j)
+{
+int r;
+r = i + j;
+return (r);
+}
