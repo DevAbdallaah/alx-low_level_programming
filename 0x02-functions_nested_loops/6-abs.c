e4da58ca7f8c85c20a1a268808b2f2b7eb427d47
DevAbdallaah <Seebawaiabdallah3@gmail.com>
2023-03-21 21:19:47 -0700

    Absolute Value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *  _abs - function to print absolute numbers
+ *  @n: is the parameter
+ *
+ *  Return: absolute numbers
+ *
+ */
+
+int _abs(int n)
+{
+return (abs(n));
+}
