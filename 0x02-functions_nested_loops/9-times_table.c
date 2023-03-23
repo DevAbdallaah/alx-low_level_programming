221c6ba52e45ba074ec2655aa8c5149b9eeefd26
DevAbdallaah <Seebawaiabdallah3@gmail.com>
2023-03-22 18:50:18 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,23 +17,6 @@ for (i = 0; i < 10; i++)
 for (j = 0; j < 10; j++)
 {
 r = i * j;
-if (r < 10)
-{
-if (j == 9)
-{
-_putchar(' ');	
-_putchar(r % 10 + '0');
-}
-else
-{
-_putchar(' ');
-_putchar(r % 10 + '0');
-_putchar(',');
-_putchar(' ');
-}
-}
-else
-{
 if (j == 9)
 {
 _putchar(r / 10 + '0');
@@ -45,7 +28,6 @@ _putchar(r / 10 + '0');
 _putchar(r % 10 + '0');
 _putchar(',');
 _putchar(' ');
-}
 }
 }
 _putchar('\n');
