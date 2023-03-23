23fdeb337af6bf3c9a1f76c9555da13ae1454017
DevAbdallaah <Seebawaiabdallah3@gmail.com>
2023-03-23 16:21:30 -0700

    0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,15 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * more_numbers - function print 0 to 14
+ * Return: 0 always
+ */
+
+
 void more_numbers(void)
 {
-int i,j;	
+int i, j;
 for (i = 0; i < 11; i++)
 {
 for (j = 0; j < 15; j++)
 {
-if (j > 9 )
-_putchar(j / 10 + '0');	
+if (j > 9)
+_putchar(j / 10 + '0');
 _putchar(j % 10 + '0');
 }
 _putchar('\n');
