64992c2c605cddfd1f50921e22bf13f4810b484b
DevAbdallaah <Seebawaiabdallah3@gmail.com>
2023-03-27 15:40:14 -0700

    Number of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+/**
+ * _strlen - function to count string
+ * @s: is the parameter
+ * Return: number of characters
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
