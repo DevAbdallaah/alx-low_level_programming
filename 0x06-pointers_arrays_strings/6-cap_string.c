780a262874758874bd481318b68355b406c6645e
DevAbdallaah <Seebawaiabdallah3@gmail.com>
2023-03-30 10:23:50 -0700

    capitalized character

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * cap_string - function to capitalize
+ * @n: is the parameter
+ * Return: string
+ */
+
+char *cap_string(char *n)
+{
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		if (n[i] == ' ')
+			n[i + 1] = n[i + 1] - 32;
+	}
+	return (n);
+}
