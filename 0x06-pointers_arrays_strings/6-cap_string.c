db8e14c91906333c83fc06318210d81e59bc5d9c
DevAbdallaah <Seebawaiabdallah3@gmail.com>
2023-03-30 10:44:24 -0700

    capitalized character

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,23 +2,22 @@
 /**
  * cap_string - function to capitalize
  * @n: is the parameter
- * Return: string
+ * Return: capitalized string
  */
 
 char *cap_string(char *n)
 {
-	int i, a;
-	a = 0;
+	int i;
 
-	if (n[a] != '\0')
-	{	
+	if (n[i] != '\0')
+	{
 		for (i = 0; n[i] != '\0'; i++)
 		{
 			if (n[i] >= 'a' && n[i] <= 'z')
 			if (i == 0 || n[i] == ' ' || n[i] == '.' || n[i] == '\n' || n[i] == '\t')
 				n[i + 1] = n[i + 1] - 32;
 		}
-		a++;
+		i++;
 	}
 	return (n);
 }
