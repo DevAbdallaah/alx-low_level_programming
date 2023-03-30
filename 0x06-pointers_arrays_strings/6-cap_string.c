e90187917e692caa05178be27fe357e406dffdd8
DevAbdallaah <Seebawaiabdallah3@gmail.com>
2023-03-30 10:28:47 -0700

    capitalized character

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *n)
 	for (i = 0; n[i] != '\0'; i++)
 	{
 		if (n[i] >= 'a' && n[i] <= 'z')
-		if (n[i] == ' ')
+		if (n[i] == ' ' || n[i] == '.' || n[i] == '\n' || n[i] == '\t')
 			n[i + 1] = n[i + 1] - 32;
 	}
 	return (n);
