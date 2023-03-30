c0e71da10dbb4f8c33622184698848f4ebe5a4ae
DevAbdallaah <Seebawaiabdallah3@gmail.com>
2023-03-29 21:33:23 -0700

    Cat with numbers

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strncat - function to concatenate with number
+ * @dest: is the destination
+ * @src: cat to the dest
+ * @n: byte for src
+ * Return: the final destination
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int destlen = 0, i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		destlen++;
+	for (i = 0; i < n; i++)
+	dest[destlen + i] = src[i];
+return (dest);
+}
