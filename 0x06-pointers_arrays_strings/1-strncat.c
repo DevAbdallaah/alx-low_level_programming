02e2f510d06f3e86cbb82935606dca4e2b78db17
DevAbdallaah <Seebawaiabdallah3@gmail.com>
2023-03-30 20:21:15 -0700

    Cat with numbers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,12 +9,11 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int destlen = 0, i;
+	int destlen = 0, i, j;
 
 	for (i = 0; dest[i] != '\0'; i++)
 		destlen++;
-	for (i = 0; i < n; i++)
-	dest[destlen + i] = src[i];
-	
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	dest[destlen + j] = src[i];
 	return (dest);
 }
