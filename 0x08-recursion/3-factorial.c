4f4aecaf8186a33dcaea35bfe7bc0038ec8d8727
DevAbdallaah <Seebawaiabdallah3@gmail.com>
2023-04-05 08:51:04 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+/**
+ * factorial - funtion to return factorial
+ * @n: parameter
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+	
+		return (n * factorial(n - 1));
+}
