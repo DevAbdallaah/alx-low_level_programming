2ada2cdda4fbacf5f3a0df13b88d8cd6efc94c44
DevAbdallaah <Seebawaiabdallah3@gmail.com>
2023-04-08 07:46:28 -0700

    argument in main

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: store count
+ * @argv: store value string
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	printf("%s\n", argv[0]);
+	return (0);
+}
