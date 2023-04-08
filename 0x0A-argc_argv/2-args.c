99995bca54b14a79748d4f2d5c92da020bf34fd0
DevAbdallaah <Seebawaiabdallah3@gmail.com>
2023-04-08 08:17:58 -0700

    Print all argument received

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main  function main
+ * @argc: store count
+ * @argv: store string values
+ * Return: 0 always
+ */
+int main (int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
