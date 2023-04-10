64ca2ae5176d82fa62326e60c1428882c85d2b4a
DevAbdallaah <Seebawaiabdallah3@gmail.com>
2023-04-10 14:48:19 -0700

    Numbers to coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,74 +1,61 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - function
+ * @argc: is the paramater
+ * @argv: is the paramater
+ * Return: 0 always
+ */
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-	int i, num;
+	int num_coins;
+	int cents;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		return 1;
 	}
-	else
+
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
 	{
-		num = atoi(argv[2]);
-		if (num < 0)
-			printf("0\n");
+		printf("0\n");
+		return 0;
+	}
+
+	num_coins = 0;
+	while (cents > 0)
+	{
+		if (cents >= 25)
+		{
+			cents -= 25;
+			num_coins++;
+		}
+		else if (cents >= 10)
+		{
+			cents -= 10;
+			num_coins++;
+		}
+		else if (cents >= 5)
+		{
+			cents -= 5;
+			num_coins++;
+		}
+		else if (cents >= 2)
+		{
+			cents -= 2;
+			num_coins++;
+		}
 		else
 		{
-			i = 0;
-			while (num > 0)
-			{
-				if (num >= 25)
-				{
-					while (num >= 25)
-					{ 	
-						num -= 25;
-						i++;
-					}			
-
-				}
-				else if (num >= 10)
-				{
-					while (num >= 10)
-					{	
-						num -= 10;
-						i++;
-					}
-					
-				}
-				else if (num >= 5)
-				{
-					while (num >= 5)
-					{
-						num -= 5;
-						i++;
-					}
-					
-				}
-				else if (num >= 2)
-				{
-					while (num >= 2)
-					{
-						num -= 2;
-						i++;
-					}
-					
-				}
-				else 
-				{
-					while (num >= 1)
-					{
-						num -= 1;
-						i++;
-					}
-					
-				}
-			}
+			cents -= 1;
+			num_coins++;
 		}
 	}
-	printf("%d\n", i);
-	return (0);
+
+	printf("%d\n", num_coins);
+	return 0;
 }
